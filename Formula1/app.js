21c3199e79880cb96d7e903c1bc4f711f6b91132
Sandu <bociasan95@gmail.com>
2022-07-22 12:18:54 +0300

    Formula CSS

--- Formula1/app.js
+++ Formula1/app.js
@@ -7,18 +7,16 @@ const container = document.querySelector('.drivers-container');
 //console.log(drivers)
 
 drivers.forEach((driver, index) =>{
-    container.innerHTML += `<div class="driver-card"> 
+    let driverCard = `<div class="driver-card"> 
             
-    <div style="display: flex; justify-content: space-between; padding: 10px 0; margin: 0; ">
-        <div style="font-size: 70; vertical-align: top; "> ${index+1} </div>
+    <div style="display: flex; justify-content: space-between;">
+        <div style="font-size: 90; vertical-align: top; "> ${index+1} </div>
         <div> 
-            <div style="font-size: 30; padding: 6px 0 0 0;">
+            <div id="points" style="font-size: 40; padding: 6px 0 0 0; text-align: right;">
                 ${driver.points}       
             </div>
 
-            <div class="pts-div">
-                PTS
-            </div>
+            <div class="pts-div">PTS</div>
 
         </div>
     </div>
@@ -27,11 +25,11 @@ drivers.forEach((driver, index) =>{
 
     <div class="space-between">
         <div style="display: flex;">
-            <canvas id="newCanvas" width="8px" height="30px" style="background: ${driver.hex}; margin-right: 5px;">
+            <canvas id="team-color" width="8px" height="30px" style="background: ${driver.hex}; margin-right: 5px;">
             </canvas>
             <div>
-                <div>${driver.firstName}</div>
-                <div>${driver.lastName}</div>
+                <div class="driver-firstname">${driver.firstName}</div>
+                <div class="driver-lastname">${driver.lastName}</div>
             </div>
         </div>
 
@@ -41,10 +39,10 @@ drivers.forEach((driver, index) =>{
 
     <div class="line"></div>
 
-    <div>${driver.team}</div> 
+    <div class="team-name">${driver.team}</div> 
 
     <div class="div-relative" >
-        <img src="${driver.image}" width=70% style="position: absolute; margin-left: 12%; ">
+        <img id="driver-img" src="${driver.image}" width=70% style=" ">
 
         <div class="driver-number">${driver.number}</div>
         <div class="plus-div"></div>
@@ -53,10 +51,32 @@ drivers.forEach((driver, index) =>{
 
     <div class="line"></div>
     <div class="space-between" style="position: relative;">
-        <button class="pts-button">+</button>
-        <button class="pts-button">-</button>
+        <button id="add-button" class="pts-button">+</button>
+        <button id="dec-button" class="pts-button">-</button>
     </div>
 
 
 </div> <!-- End card -->  `
+    
+    container.innerHTML += driverCard
+
+
 })
+
+container.querySelectorAll("#add-button").forEach((btn) => {
+    btn.addEventListener("click", (e) => increaseScore(e));
+    });
+
+container.querySelectorAll("#dec-button").forEach((btn) => {
+    btn.addEventListener("click", (e) => decreaseScore(e));
+    });
+
+const increaseScore = (event) => {
+    const score = event.target.parentElement.parentElement.querySelector("#points");
+    score.innerHTML = Number(score.innerHTML) + 1;
+  };
+
+  const decreaseScore = (event) => {
+    const score = event.target.parentElement.parentElement.querySelector("#points");
+    score.innerHTML = Number(score.innerHTML) - 1 > 0 ? Number(score.innerHTML) - 1 : 0 ;
+  };
