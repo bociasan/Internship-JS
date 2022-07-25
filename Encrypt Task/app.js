7611ccbf84993f88abc60114671870760ab614f9
Sandu <bociasan95@gmail.com>
2022-07-25 14:22:40 +0300

    Chipher  restructuring.

--- Encrypt Task/app.js
+++ Encrypt Task/app.js
@@ -1,52 +1,20 @@
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "!", "'", "_", "-", "&", "@", "#", "$", "%", "*", "(", ")", " "];
-    let message = document.querySelector(".message");
-    let code = document.querySelector(".code");
-    let encryptedMessage = document.querySelector(".encrypted-message");
-
-
-
-
-    document.querySelector(".copy-button").addEventListener("click", () => {
-
-        var copyTextarea = document.querySelector('.encrypted-message');
-        copyTextarea.focus();
-        copyTextarea.select();
-
-        try {
-            var successful = document.execCommand('copy');
-            var msg = successful ? 'successful' : 'unsuccessful';
-            console.log('Copying text command was ' + msg);
-        } catch (err) {
-            console.log('Oops, unable to copy');
+let message = document.querySelector(".message");
+let code = document.querySelector(".code");
+let encryptedMessage = document.querySelector(".encrypted-message");
+function cripter(is_raw=false){
+    let text = message.value.toUpperCase()
+    let key = code.value.toUpperCase();
+    encryptedMessage.value = "";
+    for (var i = 0; i < text.length; i++) {
+        if (is_raw) {
+            encryptedMessage.value += characters[(characters.indexOf(text[i]) + characters.indexOf(key[i % key.length])) % characters.length]
+            document.querySelector("#result").innerHTML = "Your encrypted message"
+        } else {
+            encryptedMessage.value += characters.at(characters.indexOf(text[i]) - characters.indexOf(key[i % key.length]))
+            document.querySelector("#result").innerHTML = "Your decrypted message"
         }
-    })
-
-    document.querySelector(".encrypt-button").addEventListener("click", () => {
-        document.querySelector("#result").innerHTML = "Your encrypted message:";
-
-        let text = typeof message.value === 'string' ? message.value.toUpperCase() : '';
-        let key = code.value.toUpperCase();
-        encryptedMessage.value = "";
-
-        for (var i = 0; i < text.length; i++) {
-            let j = i % key.length;
-             encryptedMessage.value += characters[(characters.indexOf(text[i]) + characters.indexOf(key[j])) % characters.length];
-        }
-    });
-
-
-
-    document.querySelector(".decrypt-button").addEventListener("click", () => {
-        document.querySelector("#result").innerHTML = "Your decrypted message:";
-        let text = typeof message.value === 'string' ? message.value.toUpperCase() : '';
-        let key = code.value.toUpperCase();
-        encryptedMessage.value = "";
-
-        for (var i = 0; i < text.length; i++) {
-            let j = i % key.length;
-
-            encryptedMessage.value += characters[characters.indexOf(text[i]) - characters.indexOf(key[j]) >= 0
-                ? characters.indexOf(text[i]) - characters.indexOf(key[j])
-                : characters.length + characters.indexOf(text[i]) - characters.indexOf(key[j])];
-        }
-    });
+    }
+}
+document.querySelector(".copy-button").addEventListener("click", () => navigator.clipboard.writeText(encryptedMessage))
+document.querySelector(".insert-button").addEventListener("click", () => message.value = encryptedMessage.value)
